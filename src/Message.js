13bdd78f393dbd30890efc79cc632b3286f26c6e
Abbas Raza <abezz098@gmail.com>
2020-11-18 01:53:06 -0800

    Update message.js

--- src/Message.js
+++ src/Message.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Message(props){
 
     return (
-    <h1> hello world:{props.counter}</h1>
+    <h1> have a peaceful life :{props.counter}</h1>
         
 
     );
